0f279817f1312c32d4cdb2151e1fafd6a58c8a5c
Sheldon Johnson <shayolden@hotmail.com>
2018-03-18 03:57:34 +1100

    WIP very specific qwtf setup for now, needs to be generalised

--- lib/discord_qstat_bot.rb
+++ lib/discord_qstat_bot.rb
@@ -1,5 +1,99 @@
 require "discord_qstat_bot/version"
+require "discordrb"
+
+class QstatRequest
+  attr_reader :status
+
+  def initialize
+    @result = JSON.parse(%x[qstat -json -P -qws qwtf.ga]).first
+  end
+
+  def address
+    @result["address"]
+  end
+
+  def map
+    @result["map"]
+  end
+
+  def numplayers
+    @result["numplayers"]
+  end
+
+  def maxplayers
+    @result["maxplayers"]
+  end
+
+  def player_table
+    players.map do |player|
+      "#{player.emoji}#{player.name}"
+    end
+  end
+
+  def players
+    @result["players"].map do |player|
+      Player.new player
+    end
+  end
+end
+
+class Player
+  CLASSES = {
+    "scout" => "<:tf_scout:424097703127941130>",
+    "sniper" => "<:tf_sniper:424097704076115978>",
+    "soldier" => "<:tf_soldier:424097704197619712>",
+    "demoman" => "<:tf_demoman:424097687739301919>",
+    "medic" => "<:tf_medic:424097695418941451>",
+    "pyro" => "<:tf_pyro:424097704403271691>",
+    "hwguy" => "<:tf_hwguy:424097694030757889>",
+    "spy" => "<:tf_spy:424097704138899466>",
+    "engineer" => "<:tf_engineer:424097694680612864>"
+  } 
+
+  def initialize(hash)
+    @player = hash
+  end
+
+  def name
+    @player["name"]
+  end
+
+  def skin
+    @player["skin"]
+  end
+
+  def klass
+    ary = CLASSES.keys.select do |key|
+      key[0..2] == short_class_name
+    end
+    ary.first
+  end
+
+  def emoji
+    CLASSES[klass]
+  end
+
+  private
+
+  def short_class_name
+    skin.split("_").last
+  end
+end
 
 module DiscordQstatBot
-  # Your code goes here...
+  raise "DISCORD_QSTAT_BOT_TOKEN environment variable not set" unless ENV["DISCORD_QSTAT_BOT_TOKEN"]
+  TOKEN = ENV["DISCORD_QSTAT_BOT_TOKEN"].strip
+
+  bot = Discordrb::Commands::CommandBot.new token: TOKEN, prefix: "!"
+
+  bot.command :server do |event|
+    request = QstatRequest.new
+
+    <<~HEREDOC
+      **#{request.address} | #{request.map} | #{request.numplayers}/#{request.maxplayers}**
+      #{request.player_table.join "\n"}
+    HEREDOC
+  end
+
+  bot.run
 end
