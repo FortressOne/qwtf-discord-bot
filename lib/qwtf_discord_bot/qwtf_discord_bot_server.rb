83166364a65946ef0533538370feee77682e5cba
Sheldon Johnson <shayolden@hotmail.com>
2019-06-24 00:24:11 +1000

    Give better response when queried in channel with no associated servers

--- lib/qwtf_discord_bot/qwtf_discord_bot_server.rb
+++ lib/qwtf_discord_bot/qwtf_discord_bot_server.rb
@@ -27,47 +27,21 @@ class QwtfDiscordBotServer < QwtfDiscordBot # :nodoc:
     end
 
     bot.command :all do |event|
-      @endpoints.each do |endpoint|
-        endpoint.channel_ids.each do |channel_id|
-          next unless event.channel.id == channel_id
-
-          qstat_request = QstatRequest.new(endpoint.address)
-          message = qstat_request.server_summary
-          embed = qstat_request.to_embed
-
-
-          if embed
-            event.channel.send_embed(message, embed)
-          else
-            event.channel.send_message(message)
-          end
-        end
-      end
-
-      return nil
-    end
-
-    bot.command :active do |event|
       endpoints_for_this_channel = @endpoints.select do |endpoint|
         endpoint.channel_ids.any? do |channel_id|
           event.channel.id == channel_id
         end
       end
 
-      qstat_requests = endpoints_for_this_channel.map do |endpoint|
-        QstatRequest.new(endpoint.address)
-      end
-
-      servers_with_players = qstat_requests.reject do |server|
-        server.is_empty?
-      end
-
-      if servers_with_players.empty?
-        event.channel.send_message("All #{event.channel.name} servers are empty")
+      if endpoints_for_this_channel.empty?
+        event.channel.send_message(
+          'There are no servers associated with this channel'
+        )
       else
-        servers_with_players.each do |server|
-          message = server.server_summary
-          embed = server.to_embed
+        endpoints_for_this_channel.each do |endpoint|
+          qstat_request = QstatRequest.new(endpoint.address)
+          message = qstat_request.server_summary
+          embed = qstat_request.to_embed
 
           if embed
             event.channel.send_embed(message, embed)
@@ -76,10 +50,43 @@ class QwtfDiscordBotServer < QwtfDiscordBot # :nodoc:
           end
         end
       end
-
-      return nil
     end
 
-    bot.run
+    return nil
   end
+
+  bot.command :active do |event|
+    endpoints_for_this_channel = @endpoints.select do |endpoint|
+      endpoint.channel_ids.any? do |channel_id|
+        event.channel.id == channel_id
+      end
+    end
+
+    qstat_requests = endpoints_for_this_channel.map do |endpoint|
+      QstatRequest.new(endpoint.address)
+    end
+
+    servers_with_players = qstat_requests.reject do |server|
+      server.is_empty?
+    end
+
+    if servers_with_players.empty?
+      event.channel.send_message("All ##{event.channel.name} servers are empty")
+    else
+      servers_with_players.each do |server|
+        message = server.server_summary
+        embed = server.to_embed
+
+        if embed
+          event.channel.send_embed(message, embed)
+        else
+          event.channel.send_message(message)
+        end
+      end
+    end
+
+    return nil
+  end
+
+  bot.run
 end
