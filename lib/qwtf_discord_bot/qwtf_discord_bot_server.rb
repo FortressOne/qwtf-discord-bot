8c5b069e5b35bbdf7e7307636f593d37408e10b6
Sheldon Johnson <shayolden@hotmail.com>
2019-06-24 19:33:50 +1000

    Fix wrong placement of end

--- lib/qwtf_discord_bot/qwtf_discord_bot_server.rb
+++ lib/qwtf_discord_bot/qwtf_discord_bot_server.rb
@@ -50,43 +50,43 @@ class QwtfDiscordBotServer < QwtfDiscordBot # :nodoc:
           end
         end
       end
+
+      return nil
     end
 
-    return nil
-  end
-
-  bot.command :active do |event|
-    endpoints_for_this_channel = @endpoints.select do |endpoint|
-      endpoint.channel_ids.any? do |channel_id|
-        event.channel.id == channel_id
-      end
-    end
-
-    qstat_requests = endpoints_for_this_channel.map do |endpoint|
-      QstatRequest.new(endpoint.address)
-    end
-
-    servers_with_players = qstat_requests.reject do |server|
-      server.is_empty?
-    end
-
-    if servers_with_players.empty?
-      event.channel.send_message("All ##{event.channel.name} servers are empty")
-    else
-      servers_with_players.each do |server|
-        message = server.server_summary
-        embed = server.to_embed
-
-        if embed
-          event.channel.send_embed(message, embed)
-        else
-          event.channel.send_message(message)
+    bot.command :active do |event|
+      endpoints_for_this_channel = @endpoints.select do |endpoint|
+        endpoint.channel_ids.any? do |channel_id|
+          event.channel.id == channel_id
         end
       end
+
+      qstat_requests = endpoints_for_this_channel.map do |endpoint|
+        QstatRequest.new(endpoint.address)
+      end
+
+      servers_with_players = qstat_requests.reject do |server|
+        server.is_empty?
+      end
+
+      if servers_with_players.empty?
+        event.channel.send_message("All ##{event.channel.name} servers are empty")
+      else
+        servers_with_players.each do |server|
+          message = server.server_summary
+          embed = server.to_embed
+
+          if embed
+            event.channel.send_embed(message, embed)
+          else
+            event.channel.send_message(message)
+          end
+        end
+      end
+
+      return nil
     end
 
-    return nil
+    bot.run
   end
-
-  bot.run
 end
