c8eede7afbe784181f22126661ed890d4abfb58f
Sheldon Johnson <shayolden@hotmail.com>
2020-08-19 00:05:29 +1000

    Add redis into the mix

--- lib/qwtf_discord_bot/qwtf_discord_bot_pug.rb
+++ lib/qwtf_discord_bot/qwtf_discord_bot_pug.rb
@@ -1,4 +1,8 @@
+require "redis"
+
 class QwtfDiscordBotPug
+  FOUR_HOURS = 4 * 60 * 60
+
   def run
     bot = Discordrb::Commands::CommandBot.new(
       token: QwtfDiscordBot.config.token,
@@ -7,19 +11,31 @@ class QwtfDiscordBotPug
     )
 
     bot.command :join do |event, *args|
-      # if pug_active?
-      #   join_pug
-      # else
-      #   create_pug
-      # end
+      pug = ["pug", event.channel].join(":")
+
+      message = if redis.get(pug)
+        "#{event.user.username} joins the PUG."
+      else
+        redis.set(pug, Time.now)
+        redis.expire(pug, FOUR_HOURS)
+        "#{event.user.username} starts a PUG. `!join` to join."
+      end
+
+      player = [pug, event.user.id].join(":")
+      redis.set(player, Time.now)
 
       # start_pug if pug_full?
 
-      message = "#{event.user.username} has joined"
       event.channel.send_message(message)
       puts message
     end
 
     bot.run
+  end
+
+  private
+
+  def redis
+    @redis ||= Redis.new
   end
 end
