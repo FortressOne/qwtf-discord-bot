2a2c6cb6405ef8abb4b5d6f44e1c470880fcf3a9
Sheldon Johnson <shayolden@hotmail.com>
2023-04-09 17:58:08 +1000

    Add dynamic timer, use mapname instead of emoji when declaring winner

--- lib/qwtf_discord_bot/qwtf_discord_bot_vote.rb
+++ lib/qwtf_discord_bot/qwtf_discord_bot_vote.rb
@@ -3,7 +3,6 @@ require 'pug'
 class QwtfDiscordBotVote
   TIMER = 10
   REACTION_EMOJIS = ["🍏", "🍊", "🍋", "❌"]
-  MSG_SNIPPET_DELIMITER = ' · '
 
   def run
     bot = Discordrb::Commands::CommandBot.new(
@@ -28,43 +27,46 @@ class QwtfDiscordBotVote
     map_names_mutex = Mutex.new
     map_names = []
 
+    embed_mutex = Mutex.new
+
     # Register the vote when someone adds a reaction
     bot.reaction_add do |event|
+      if !pug(event).joined?(event.user.id)
+        next
+      end
+
       if vote_thread&.alive? && event.message.id == @vote_message.id
         emoji = event.emoji.to_s
-        if votes.key?(emoji)
+        map_name = REACTION_EMOJIS.zip(map_names).to_h[emoji]
+
+        if votes.key?(map_name)
           user_id = event.user.id
           unless event.user.current_bot?
-            # Register the vote
-            votes[emoji] += 1
+            votes[map_name] += 1
+            majority = pug(event).teamsize + 1
 
-            # Calculate the majority threshold
-            majority = teamsize(event) + 1
-
-            if votes[emoji] >= majority
-              # A map has reached the majority of votes
+            if votes[map_name] >= majority
               should_end_voting_mutex.synchronize { should_end_voting = true }
-              announce_winner(event, [maps[emoji], votes[emoji]])
+              announce_winner(event, [maps[emoji], votes[map_name]])
             end
           end
         end
       end
     end
 
-    # Add a command to the bot for starting the voting process
     bot.command(:vote, description: 'Start the voting process') do |event|
       if vote_thread.nil? || !vote_thread.alive?
-        # Reset the votes and the shared variable
         votes.clear
         should_end_voting_mutex.synchronize { should_end_voting = false }
 
-        # Request map suggestions
-        uri = URI([ENV['RESULTS_API_URL'], 'map_suggestions'].join('/'))
+        uri = URI([ENV['RESULTS_API_URL'], 'map_suggestions', 'vote'].join('/'))
         req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
 
         req.body = {
-          map_suggestion: { discord_channel_id: event.channel.id },
-          size: 3
+          map_suggestion: {
+            channel_id: event.channel.id,
+            for_teamsize: pug(event).teamsize,
+          }
         }.to_json
 
         is_https = uri.scheme == "https"
@@ -78,22 +80,40 @@ class QwtfDiscordBotVote
         maps = REACTION_EMOJIS.zip(map_names).to_h
 
         # Create the vote message with reaction options
-
         embed = Discordrb::Webhooks::Embed.new
         embed.description = maps.map { |map| "#{map[0]} #{map[1]}" }.join("\n")
-        message = "Voting started. #{TIMER} seconds."
+        embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "")
+
+        message = "Joined players, choose your maps"
         @vote_message = event.channel.send_embed(message, embed).tap do
           puts(embed.description)
         end
 
         # Voting process
         vote_thread = Thread.new do
-          TIMER.times do
+          TIMER.times do |i|
             break if should_end_voting
+
+            embed_mutex.synchronize do
+              embed.footer = Discordrb::Webhooks::EmbedFooter.new(
+                text: "#{TIMER - i} seconds remaining"
+              )
+            end
+
+            @vote_message.edit(message, embed)
+
             sleep 1
           end
 
           if !should_end_voting
+            embed_mutex.synchronize do
+              embed.footer = Discordrb::Webhooks::EmbedFooter.new(
+                text: "0 seconds remaining"
+              )
+            end
+
+            @vote_message.edit(message, embed)
+
             # Voting time ended, announce the winner
             winner = votes.max_by { |_, v| v }
             announce_winner(event, winner)
@@ -102,7 +122,7 @@ class QwtfDiscordBotVote
 
         maps.each do |emoji, map_name|
           @vote_message.react(emoji)
-          votes[emoji] = 0
+          votes[map_name] = 0
         end
       else
         event.respond('Voting is already in progress!')
@@ -111,15 +131,46 @@ class QwtfDiscordBotVote
       nil
     end
 
+    bot.command :map do |event, *args|
+      uri = URI([ENV['RESULTS_API_URL'], 'map_suggestions'].join('/'))
+      req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+
+      req.body = {
+        map_suggestion: {
+          discord_channel_id: event.channel.id,
+          discord_player_id: event.user.id,
+          discord_player_name: evenr.user.nickname
+        }
+      }.to_json
+
+      is_https = uri.scheme == "https"
+
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: is_https) do |http|
+        http.request(req)
+      end
+
+      body = JSON.parse(res.body)
+
+      description = if body
+                      "How about #{body}?"
+                    else
+                      "I'm out of ideas, you choose."
+                    end
+
+      send_embedded_message(
+        description: description,
+        channel: event.channel
+      )
+    end
+
+
     # Function to announce the winner
     def announce_winner(event, winner)
       event.respond("The winner is #{winner[0]} with #{winner[1]} votes.")
     end
 
-    def teamsize(event)
-      e = EventDecorator.new(event)
-      pug = Pug.for(e.channel_id)
-      pug.teamsize
+    def pug(event)
+      Pug.for(event.channel.id)
     end
 
     bot.run
